2616da5b2331e10db444b3ee7c6e3e65b35b0a39
Manuel Moya Vadillo <manuel.moyavadillo2@gmail.com>
2024-08-09 01:48:35 +0200

    feat: added some css code and test mui icons

--- proyecto2frontend/src/app/page.tsx
+++ proyecto2frontend/src/app/page.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
+import * as React from 'react';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 
 export default function Home() {
   return (
-    <main>
-      <h1>ReserHub</h1>
-      <h4>The best service</h4>
-    </main>
+      <main className={'MainContainer'}>
+        <header className={'HeaderContainer'}>
+            <div className={'HeaderContainerContent'}>
+                <nav className={'NavBarContainer'}>
+                    <h1 className={'NavTitle'}>ReserHub</h1>
+                    <div className={'NavBarItemsContainer'}>
+                        <ul>
+                            <li>About</li>
+                            <li>Crew</li>
+                            <li>Contact</li>
+                            <li>
+                                <DehazeIcon />
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <div className={'HeaderContainerVideoContainer'}>
+
+            </div>
+        </header>
+      </main>
   );
 }
